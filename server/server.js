3d95996d45cb3aeb61e8ed02a2c808f92858b35c
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-08-30 17:47:10 +0300

    fix server

--- server/server.js
+++ server/server.js
@@ -3,26 +3,37 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+const path = './data.json';
+function checkHttps(request, response, next) {
+  // Check the protocol — if http, redirect to https.
+  if (request.get("X-Forwarded-Proto").indexOf("https") != -1) {
+    return next();
+  } else {
+    response.redirect("https://" + request.hostname + request.url);
+  }
+}
+
+app.all("*", checkHttps)
 app.use(express.static('/build/index.html'));
 
 app.get('/api/tickets', (request, response) => {
-  const data = fs.readFileSync('./data.json');
+  const data = fs.readFileSync(path);
   const tickets = JSON.parse(data);
 
   if (request.query.searchText) {
     const queryParam = request.query.searchText.toLowerCase();
     const newTickets = tickets.filter((ticket) => {
-      const filteredTicket = ticket.title.toLowerCase();
+      const filterTickets = ticket.title.toLowerCase();
       // filter by labels. checks if "#" is the first letter
       if (queryParam.indexOf('#') === 0) {
-        const labelQueryParam = queryParam.replace('#', '');
         if (!ticket.labels) {
           return false;
         }
+        const filterLabels = ticket.labels.map((element) => element.toLowerCase());
         // check for every label in labels if it contains the queryParam
-        return ticket.labels.some((label) => label.toLowerCase().includes(labelQueryParam));
+        return filterLabels.some((label) => label.includes(queryParam.replace('#', '')));
       }
-      return filteredTicket.includes(queryParam);
+      return filterTickets.includes(queryParam);
     });
     response.send(newTickets);
   } else {
@@ -75,5 +86,23 @@ app.post('/api/tickets/favorite/:ticketId/:favorite', (request, response) => {
     console.log('The file has been saved!');
   });
 });
+let port;
+console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  port = process.env.PORT || 3000;
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+} else {
+  port = 3001;
+  console.log("⚠️ Not seeing your changes as you develop?");
+  console.log(
+    "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
+  );
+}
 
-module.exports = app;
+// Start the listener!
+const listener = app.listen(port, () => {
+  console.log("❇️ Express server is running on port", listener.address().port);
+});
